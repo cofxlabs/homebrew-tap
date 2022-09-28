78f6abf8ef2c4f379271484bff508ac735d6fd2e
skoowu <skoowu.fancy@gmail.com>
2022-09-28 14:42:58 +0800

    fix: fix syntax error
    Signed-off-by: skoowu <skoowu.fancy@gmail.com>

--- cofx.rb
+++ cofx.rb
@@ -7,12 +7,11 @@ class Cofx < Formula
   url "https://github.com/cofxlabs/cofx/releases/download/v0.0.1/cofx-darwin-amd64.tar.gz"
   sha256 "b0e869e02d20edadd31281008f11ef7c561a8006ad258d54203a3022014a5bfc"
   license "Apache-2.0"
-
-  # depends_on "cmake" => :build
+  version "v0.0.1"
 
   def install
     bin.install "cofx"
-    bin/install Dir("flowls")
+    bin/install Dir["flowls"]
   end
 
 end
