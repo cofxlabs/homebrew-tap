92c2082c829b5698e3d3fcca8b29b70edba5f690
skoowu <skoowu.fancy@gmail.com>
2022-09-28 14:39:50 +0800

    feature: add cofx.rb file
    Signed-off-by: skoowu <skoowu.fancy@gmail.com>

---
+++ cofx.rb
@@ -0,0 +1,18 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Cofx < Formula
+  desc "CoFx is a powerful automation workflow engine based on low code programming language"
+  homepage ""
+  url "https://github.com/cofxlabs/cofx/releases/download/v0.0.1/cofx-darwin-amd64.tar.gz"
+  sha256 "b0e869e02d20edadd31281008f11ef7c561a8006ad258d54203a3022014a5bfc"
+  license "Apache-2.0"
+
+  # depends_on "cmake" => :build
+
+  def install
+    bin.install "cofx"
+    bin/install Dir("flowls")
+  end
+
+end
